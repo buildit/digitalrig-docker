816a418a0448201b63a5740f83b7bee3c6f3439d
rsafronov <electroma@gmail.com>
2017-01-24 19:17:11 -0500

    Cleanup + restructure

---
+++ helm/ec2_rig_install.sh
@@ -0,0 +1,25 @@
+# Install tiller
+helm init
+
+#openvpn
+helm delete --purge openvpn
+helm install ../../charts/digitalrig/openvpn-k8s --namespace default --name openvpn -f ./ec2_vars/openvpn.yaml
+# helm upgrade openvpn ../../charts/digitalrig/openvpn-k8s --namespace default -f ./ec2_vars/openvpn.yaml
+
+# internal proxy
+helm delete --purge traefik-int
+helm install ../../charts/stable/traefik --namespace kube-system --name traefik-int -f ./ec2_vars/traefik-internal.yaml
+
+# at this point need to create/adjust route53 wildcard private record (*.riglet) to make traefik work as internal proxy
+
+# public proxy
+helm delete --purge traefik-pub
+helm install ../../charts/stable/traefik --namespace kube-system --name traefik-pub -f ./ec2_vars/traefik-public.yaml
+
+# at this point need to create/adjust route53 wildcard record (*.apps.[domain]) to make traefik work as an edge proxy
+
+#jenkins
+helm delete --purge jenkins
+helm install ../../charts/stable/jenkins --namespace default --name jenkins -f ./ec2_vars/jenkins.yaml
+
+# cleanup helm delete --purge `helm ls -q --all`
--- helm/rig_install.sh
+++ helm/rig_install.sh
@@ -1,22 +1,25 @@
+# Install tiller
+helm init
+
 # internal proxy
 helm delete --purge traefik-int
-helm install ../../../charts/stable/traefik --namespace kube-system --name traefik-int -f ./vars/traefik-internal.yaml
+helm install ../../charts/stable/traefik --namespace kube-system --name traefik-int -f ./vars/traefik-internal.yaml
 minikube service -n kube-system  --format "http://traefik.kube.local:{{.Port}}" traefik-int-traefik
 
 # public proxy
 helm delete --purge traefik-pub
-helm install ../../../charts/stable/traefik --namespace kube-system --name traefik-pub -f ./vars/traefik-public.yaml
+helm install ../../charts/stable/traefik --namespace kube-system --name traefik-pub -f ./vars/traefik-public.yaml
 minikube service -n kube-system  --format "http://traefik-public.kube.local:{{.Port}}" traefik-int-traefik
 
 # to make jenkins data visible on host: minikube ssh >> sudo ln -s /Users/romansafronov/dev/tmp/pv /tmp/hostpath_pv
 
 #jenkins
 helm delete --purge jenkins
-helm install ../../../charts/stable/jenkins --namespace default --name jenkins -f ./vars/jenkins.yaml
+helm install ../../charts/stable/jenkins --namespace default --name jenkins -f ./vars/jenkins.yaml
 
 #openvpn
 helm delete --purge openvpn
-helm install ../../../charts/digitalrig/openvpn-k8s --namespace default --name openvpn -f ./vars/openvpn.yaml
+helm install ../../charts/digitalrig/openvpn-k8s --namespace default --name openvpn -f ./vars/openvpn.yaml
 # helm upgrade openvpn ../../../charts/digitalrig/openvpn-k8s --namespace default -f ./vars/openvpn.yaml
 minikube service -n default --format "remote {{.IP}} {{.Port}}" openvpn-openvpn
 
