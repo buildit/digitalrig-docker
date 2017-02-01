7a0ccbb94b7b12dd330cd49d24647fde36bcb008
rsafronov <electroma@gmail.com>
2017-02-01 13:31:21 -0500

    Minor updates of rig install script + readme

--- helm/rig_install.sh
+++ helm/rig_install.sh
@@ -4,12 +4,10 @@ helm init
 # internal proxy
 helm delete --purge traefik-int
 helm install ../../charts/stable/traefik --namespace kube-system --name traefik-int -f ./vars/traefik-internal.yaml
-minikube service -n kube-system  --format "http://traefik.kube.local:{{.Port}}" traefik-int-traefik
 
 # public proxy
 helm delete --purge traefik-pub
 helm install ../../charts/stable/traefik --namespace kube-system --name traefik-pub -f ./vars/traefik-public.yaml
-minikube service -n kube-system  --format "http://traefik-public.kube.local:{{.Port}}" traefik-int-traefik
 
 # to make jenkins data visible on host: minikube ssh >> sudo ln -s /Users/romansafronov/dev/tmp/pv /tmp/hostpath_pv
 
@@ -24,9 +22,9 @@ helm install ../../charts/digitalrig/samba-ad-dc/ -n ad-dc -f ./vars/ad-dc.yaml
 #openvpn
 helm delete --purge openvpn
 helm install ../../charts/digitalrig/openvpn-k8s --namespace default --name openvpn -f ./vars/openvpn.yaml
-# helm upgrade openvpn ../../../charts/digitalrig/openvpn-k8s --namespace default -f ./vars/openvpn.yaml
-minikube service -n default --format "remote {{.IP}} {{.Port}}" openvpn-openvpn
 
+echo "OpenVPN client configuration"
+minikube service -n default --format "{{.IP}}:{{.Port}}" openvpn-openvpn
+
+minikube service -n kube-system  --format "http://traefik.kube.local:{{.Port}}" traefik-int-traefik
 # cleanup helm delete --purge `helm ls -q --all`
-
-helm install ../../charts/stable/jenkins --namespace default --name jenkins2 -f ./vars/jenkins2.yaml
