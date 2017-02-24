eda0b6323a2ed53e972138bc248194fd9c666f70
rsafronov <electroma@gmail.com>
2017-02-24 17:32:20 -0500

    HA KOPS config

--- ec2/kops.sh
+++ ec2/kops.sh
@@ -1,13 +1,14 @@
-export KOPS_STATE_STORE=s3://kops.store.k8s.riglet.io
+export KOPS_STATE_STORE=s3://kops.store.rsafronov.k8s.riglet.io
 kops create cluster \
         --cloud=aws \
         --topology private \
         --bastion \
-        --zones us-east-1b \
-        --master-zones us-east-1b \
+        --zones us-east-1a,us-east-1b,us-east-1c \
+        --master-count 3 \
+        --master-zones us-east-1a,us-east-1b,us-east-1c \
+        --master-size t2.small \
         --node-count 3 \
-        --node-size t2.medium \
-        --master-size t2.medium \
+        --node-size m4.large \
         --dns-zone=k8s.riglet.io \
         --channel alpha \
         --networking weave \
